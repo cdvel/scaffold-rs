b3286d3f3140f157617e1a4b42c0e432a9ffce4e
Cesar D. Velandia <cesar@velandia.co>
2025-03-18 12:18:20 +0800

    feat(src): add boilerplate logging

---
+++ template/src/logging.rs
@@ -0,0 +1,10 @@
+pub fn setup(verbose: bool) {
+    let filter = if verbose { "debug" } else { "info" };
+
+    tracing_subscriber::fmt()
+        .with_env_filter(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| tracing_subscriber::EnvFilter::new(filter)),
+        )
+        .init();
+}
